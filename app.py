62b876d3994a4a077547f3cc68b3cb774517761d
Samir Mohsin <samir.smohsin@utexas.edu>
2022-04-15 00:31:30 -0500

    re-added port

--- app.py
+++ app.py
@@ -303,4 +303,4 @@ def not_found(e):
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
