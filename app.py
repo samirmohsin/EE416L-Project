1673e97133f130a5f9a244f131651e4dab3535ad
Samir Mohsin <samir.smohsin@utexas.edu>
2022-04-15 14:06:10 -0500

    comment out errorhandler 404

--- app.py
+++ app.py
@@ -298,9 +298,9 @@ def getMetadata():
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
+#@app.errorhandler(404)
+#def not_found(e):
+  #  return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
